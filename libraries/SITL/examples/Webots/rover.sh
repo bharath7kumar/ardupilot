0c231083aead7aa3a7c8719f8fe03f7a6cd91151
Bharath Kumar <kumar7bharath@gmail.com>
2020-06-30 15:42:51 +0530

    Changes in Webots bash scripts

--- libraries/SITL/examples/Webots/rover.sh
+++ libraries/SITL/examples/Webots/rover.sh
@@ -2,4 +2,8 @@
 
 # assume we start the script from the root directory
 ROOTDIR=$PWD
-$PWD/Tools/autotest/sim_vehicle.py -v Rover -w --model webots-rover --add-param-file=libraries/SITL/examples/Webots/rover.parm 
+#$PWD/Tools/autotest/sim_vehicle.py -v Rover -w --model webots-rover --add-param-file=libraries/SITL/examples/Webots/rover.parm 
+
+$PWD/Tools/autotest/sim_vehicle.py --console -v Rover -w --model webots-rover --add-param-file=libraries/SITL/examples/Webots/rover.parm 
+
+#$PWD/Tools/autotest/sim_vehicle.py --console -v Rover -w --add-param-file=libraries/SITL/examples/Webots/rover.parm 
